5a3fd5a9eb4d38b0550db36866cffd5fa740c3c9
kimwehnes21 <48588176+kimwehnes21@users.noreply.github.com>
2020-02-19 14:06:39 +0100

    test-commit

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Matches
+                    Matches-test
                 </div>
 
                 <div class="links">
