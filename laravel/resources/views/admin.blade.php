e4be6291c2199298775fb6c2a80e3cd2215c5144
kimwehnes21 <48588176+kimwehnes21@users.noreply.github.com>
2020-02-20 14:21:19 +0100

    view admin.blade angefangen

--- laravel/resources/views/admin.blade.php
+++ laravel/resources/views/admin.blade.php
@@ -5,16 +5,30 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Admin Dashboard</div>
+                <div class="card-header">Matches</div>
 
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
+
                     @endif
 
-                    You are logged in as Admin!
+                    <a href="/activitymatch"><div class="">Aktivitäten</div></a>
+                    <a href="/musicmatch"><div class="">Musikrichtung</div></a>
+
+                    <h3>Neueste Matches</h3>
+                    <div class="">
+
+                    </div>
+
+                    <h3>Alle Matches</h3>
+                    <div class="">
+
+
+                    </div>
+                    <a class="btn btn-primary" href="">Mehr laden</a>
                 </div>
             </div>
         </div>
