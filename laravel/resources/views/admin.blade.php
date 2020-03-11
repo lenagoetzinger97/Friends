6caf41e482440a93c782604dbfd46afb9406b992
kimwehnes21 <48588176+kimwehnes21@users.noreply.github.com>
2020-03-11 14:35:14 +0100

    Eventer Home Seite Anfang
    erster Teil der Eventer Seite in noch nicht hübsch

--- laravel/resources/views/admin.blade.php
+++ laravel/resources/views/admin.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Matches</div>
+                <div class="card-header">Events</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -15,12 +15,40 @@
 
                     @endif
 
-                    <a href="/activitymatch"><div class="">Aktivitäten</div></a>
-                    <a href="/musicmatch"><div class="">Musikrichtung</div></a>
-
-                    <h3>Neueste Matches</h3>
+                    <h3>Neues Event erstellen</h3>
                     <div class="">
+                      <p>Lege neue Events an und teile der Welt mit, was bei dir cooles abgeht!</p>
+                      <a href="#" class="btn btn-primary btn-lg disabled" tabindex="-1" role="button" aria-disabled="true">Event anlegen</a>
+                    </div>
 
+                    <h3>Aktuelle Events im Überblick</h3>
+                    <div class="">
+                      <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
+
+                      <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
+
+                      <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
                     </div>
 
                     <h3>Alle Matches</h3>
