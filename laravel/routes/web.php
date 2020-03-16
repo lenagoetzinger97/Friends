6b37cb2cf7d3dbf1131a40ac99c439e844f2335e
Lena <lenagoetzinger97@gmail.com>
2020-03-16 12:00:51 +0100

    Test

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -22,18 +22,3 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin/login','Auth\AdminLoginController@showLoginForm')->name('admin.login');
 Route::post('/admin/login','Auth\AdminLoginController@login')->name('admin.login.submit');
 Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
-
-
-
-
-// von Lena Frerichs hinzugefügt am 10.03.2020 14:11 Uhr:
-
-Auth::routes();
-
-Route::get('/matches', 'MatchController@index')->name('match');
-Route::get('/activitymatch', 'AktivitaetController@index')->name('aktivitaet');
-Route::get('/musicmatch', 'MusikgeschmackController@index')->name('musikgeschmack');
-Route::get('/profile', 'ProfilController@index')->name('profil');
-Route::get('/editprofile', 'ProfilEditController@index')->name('profilEdit');
-Route::post('/editprofile', 'ProfilEditController@index')->name('profilEdit');
-Route::get('/ausgabe', 'AusgabeController@index')->name('ausgabe');
