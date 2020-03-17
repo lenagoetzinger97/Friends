9850fce4df68cc719bc281e13c6f75a2a865f288
Lena <lenagoetzinger97@gmail.com>
2020-03-18 00:21:30 +0100

    Update userprofil.blade.php

--- laravel/resources/views/user/userprofil.blade.php
+++ laravel/resources/views/user/userprofil.blade.php
@@ -18,9 +18,9 @@
                             <div style="background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4YpX3wpf0VlhoxvYamPEvfvPSaYXRMZAeXmNtAu-9c6Kwbosb&s'); background-position: center; width: 130px; height: 130px; border-radius: 100px;"></div>
                         </div>
                         <div class="col-md-6">
-                            {{ Auth::user()->name }}<br>
-                            <h3 id="h3-profile">{{ Auth::user()->name }}</h3>
-                            26 | Heilbronn
+                        </br></br>
+                            {{ Auth::user()->name }}</br>
+                            Alter | Wohnort
                         </div>
                         <div class="col-md-3">
                             <a href="editprofile" class="edit-profile"><img src="https://static.thenounproject.com/png/33660-200.png" width="30"></a>
@@ -29,7 +29,7 @@
                   </br> </br>
                     <div class="row">
                         <div class="col">
-                            <h1>Aktivities:</h1>
+                            <h2>Aktivities:</h2>
                             <span class="tag">Schwimmen</span>
                         </div>
                     </div>
@@ -38,7 +38,7 @@
 
                     <div class="row">
                         <div class="col">
-                            <h1>Musics:</h1>
+                            <h2>Musics:</h2>
                             <span class="tag">Rock</span>
                         </div>
                     </div>
